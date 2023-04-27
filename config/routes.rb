098287180c5155c663c7a5e9b7c5c3f27543f2db
sammyremi <adebayorsamuelas@gmail.com>
2023-04-27 17:10:40 +0100

    Post model migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+
+  root "projects#index"
+  resources :projects
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
